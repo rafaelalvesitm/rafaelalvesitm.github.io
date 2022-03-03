24802e064a29762b88af30e3bc23b9a19adb2e66
rafaelalvesitm <rgomesal@hotmail.com>
2022-03-03 15:13:42 -0300

    Pequenas alterações no estilo do site

---
+++ assets/js/navbar_movement.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Shorten the navbar after scrolling a little bit down
+  $(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar").addClass("top-nav-short");
+    } else {
+      $(".navbar").removeClass("top-nav-short");
+    }
+  });
+
+  // On mobile, hide the avatar when expanding the navbar menu
+  $('#main-navbar').on('show.bs.collapse', function () {
+    $(".navbar").addClass("top-nav-expanded");
+  });
+  $('#main-navbar').on('hidden.bs.collapse', function () {
+    $(".navbar").removeClass("top-nav-expanded");
+  });
+
+});
